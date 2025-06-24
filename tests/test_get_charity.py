c3db2242d5301edf69203fa03c165288bd0ddaf8
Bijay <bjnakarmi1996@gmail.com>
2025-06-25 05:35:29 +0545

    Selenium UI + extract token + API request in test_get_charity

--- tests/test_get_charity.py
+++ tests/test_get_charity.py
@@ -1,9 +1,26 @@
+import requests
 from utils import api_helpers
+from utils.config import CONFIG
+from pages.login_page import LoginPage
+import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_get_pending_charity():
+
+@pytest.mark.parametrize('email, password', [(CONFIG['login_email'], CONFIG['login_password'])])
+def test_get_pending_charity(browser,email,password):
+    browser.get(CONFIG['base_url'])
+    login_page = LoginPage(browser)
+    login_page.set_email(email)
+    login_page.set_password(password)
+    login_page.click_login()
+    WebDriverWait(browser, timeout=10).until(
+        EC.url_contains('https://dashboard.dev.piiink.org/dashboard')
+    )
+    token = browser.execute_script("return window.localStorage.getItem('authToken');")
     header = {
-        'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjY0LCJyb2xlIjoiY291bnRyeU93bmVyIiwicHJpbWFyeUNvbnRhY3ROYW1lIjoiSmFtZXMgTGllciIsImlhdCI6MTc1MDcxMjE2NCwiZXhwIjoxNzU4NDg4MTY0fQ.W3wJt_z249ALfsBAiZ-rn50E0QbU6dHooExo7ch6dkQ'
+        'Authorization' : f"Bearer {token}"
     }
     response = api_helpers.get_all_charity(header)
     data = response.json()
